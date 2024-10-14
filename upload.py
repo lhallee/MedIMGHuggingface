57e6164e31d3410d6764690564997e134062ced0
Logan Hallee <lhallee@udel.edu>
2024-10-14 13:26:58 -0400

    argparse and dicom added

--- upload.py
+++ upload.py
@@ -6,132 +6,146 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pydicom
+import argparse
 from huggingface_hub import HfApi, create_repo
 from tqdm.auto import tqdm
 from PIL import Image
 from glob import glob
 
 
-repo_id = 'GleghornLab/MedIMG'
-#create_repo(repo_id=repo_id, repo_type='dataset', exist_ok=True)
-folders = [ex.replace('\\', '/') for ex in glob('E:/medimg/*') if not ex.endswith('z01')]
-print(folders)
-image_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.dcm')
-#medical_image_extensions = ('.mhd', '.nii')
-#image_extensions = ('.dcm',)
-medical_image_extensions = ('.mhd',)
+def main(args, api):
+    repo_id = args.repo_id
+    image_extensions = args.image_extensions
+    medical_image_extensions = args.medical_image_extensions
+    parquet_file = args.parquet_file
+    batch_size = args.batch_size
+    counter = args.counter
+    split = args.split
+    image_file_paths, data = [], []
+    # Remove existing parquet file if it exists
+    if os.path.exists(parquet_file):
+        os.remove(parquet_file)
 
-api = HfApi()
-batch_size = 10000
-image_file_paths = []
-parquet_file = 'images.parquet'
-data = []
-split, counter = 1, 0  # change split based on where starting
-
-# Remove existing parquet file if it exists
-if os.path.exists(parquet_file):
-    os.remove(parquet_file)
-
-# Collect image file paths
-for folder in folders:
-    folder_file_paths = []
-    for dirpath, dirnames, filenames in os.walk(folder):
-        for filename in filenames:
-            file_path = os.path.join(dirpath, filename)
-            # Check if the file has a known image extension
-            if filename.lower().endswith(image_extensions + medical_image_extensions):
-                folder_file_paths.append(file_path)
-            elif '.raw' in filename.lower():
-                # these are read by .mhd
-                continue
-            else:
-                # Try to open files without a recognized extension to check if they are images
-                try:
-                    with open(file_path, 'rb') as f:
-                        img_bytes = f.read()
-                    # Verify the image can be opened
-                    Image.open(io.BytesIO(img_bytes))
+    # Collect image file paths
+    for folder in folders:
+        folder_file_paths = []
+        for dirpath, dirnames, filenames in os.walk(folder):
+            for filename in filenames:
+                file_path = os.path.join(dirpath, filename)
+                # Check if the file has a known image extension
+                if filename.lower().endswith(image_extensions + medical_image_extensions):
                     folder_file_paths.append(file_path)
-                    #print(f"Found image file without known extension: {file_path}")
-                except Exception as e:
-                    pass
-    print(f"Found {len(folder_file_paths)} image files in {folder}")
-    image_file_paths.extend(folder_file_paths)
-print(f"Found {len(image_file_paths)} image files in total")
-
-
-# Process images in batches
-for image_path in tqdm(image_file_paths, desc='Reading'):
-    try:
-        if 'label' in image_path.lower():
-            continue
-        if image_path.lower().endswith('.dcm'):
-            # Handle DICOM files separately
-            try:
-                dcm_data = pydicom.dcmread(image_path)
-                image_array = dcm_data.pixel_array
-                normalized_image = ((image_array - image_array.min()) / (image_array.max() - image_array.min()) * 255).astype(np.uint8)
-                pil_image = Image.fromarray(normalized_image)
-                img_byte_arr = io.BytesIO()
-                pil_image.save(img_byte_arr, format='PNG')
-                img_bytes = img_byte_arr.getvalue()
-                file_name = f"{split}_" + image_path.split('medimg/')[-1].replace('\\', '/')
-                data.append({
-                    'img_name': file_name,
-                    'image': img_bytes,
-                })
-                counter += 1
-            except Exception as e:
-                print(f"Error processing DICOM {image_path}: {e}")
-
-        elif image_path.lower().endswith(medical_image_extensions):
-            try:
-                # .mdh files read the .raw with GetArrayFromImage
-                image_array = sitk.GetArrayFromImage(sitk.ReadImage(image_path))
-                if '.mhd' in image_path.lower():
-                    print(image_array.shape)
-                num_slices = image_array.shape[0]
-                if num_slices > 10:
-                    indices = np.linspace(0, num_slices - 1, 10).astype(int)
+                elif '.raw' in filename.lower():
+                    # these are read by .mhd
+                    continue
                 else:
-                    indices = np.arange(num_slices)
-                for index in indices:
-                    slice_data = image_array[index, :, :]
-                    normalized_slice = ((slice_data - slice_data.min()) / (slice_data.max() - slice_data.min()) * 255).astype(np.uint8)
-                    pil_image = Image.fromarray(normalized_slice)
+                    # Try to open files without a recognized extension to check if they are images
+                    try:
+                        with open(file_path, 'rb') as f:
+                            img_bytes = f.read()
+                        # Verify the image can be opened
+                        Image.open(io.BytesIO(img_bytes))
+                        folder_file_paths.append(file_path)
+                        #print(f"Found image file without known extension: {file_path}")
+                    except Exception as e:
+                        pass
+        print(f"Found {len(folder_file_paths)} image files in {folder}")
+        image_file_paths.extend(folder_file_paths)
+    print(f"Found {len(image_file_paths)} image files in total")
+
+
+    # Process images in batches
+    for image_path in tqdm(image_file_paths, desc='Reading'):
+        try:
+            if 'label' in image_path.lower():
+                continue
+            if image_path.lower().endswith('.dcm'):
+                # Handle DICOM files separately
+                try:
+                    dcm_data = pydicom.dcmread(image_path)
+                    image_array = dcm_data.pixel_array
+                    normalized_image = ((image_array - image_array.min()) / (image_array.max() - image_array.min()) * 255).astype(np.uint8)
+                    pil_image = Image.fromarray(normalized_image)
                     img_byte_arr = io.BytesIO()
                     pil_image.save(img_byte_arr, format='PNG')
                     img_bytes = img_byte_arr.getvalue()
-                    file_name = (
-                        f"{split}_"
-                        + image_path.split('medimg/')[-1].replace('\\', '/') + f'_slice{index}'
-                    )
+                    file_name = f"{split}_" + image_path.split('medimg/')[-1].replace('\\', '/')
                     data.append({
                         'img_name': file_name,
                         'image': img_bytes,
                     })
                     counter += 1
-            except Exception as e:
-                print(f"Error processing NIfTI {image_path}: {e}")
+                except Exception as e:
+                    print(f"Error processing DICOM {image_path}: {e}")
 
-        elif image_path.lower().endswith(image_extensions):
-            # Handle regular image files
-            with open(image_path, 'rb') as f:
-                img_bytes = f.read()
-            file_name = str(split) + '_' + image_path.split('medimg/')[-1].replace('\\', '/')
-            data.append({
-                'img_name': file_name,
-                'image': img_bytes,
-            })
-            counter += 1
+            elif image_path.lower().endswith(medical_image_extensions):
+                try:
+                    # .mdh files read the .raw with GetArrayFromImage
+                    image_array = sitk.GetArrayFromImage(sitk.ReadImage(image_path))
+                    if '.mhd' in image_path.lower():
+                        print(image_array.shape)
+                    num_slices = image_array.shape[0]
+                    if num_slices > 10:
+                        indices = np.linspace(0, num_slices - 1, 10).astype(int)
+                    else:
+                        indices = np.arange(num_slices)
+                    for index in indices:
+                        slice_data = image_array[index, :, :]
+                        normalized_slice = ((slice_data - slice_data.min()) / (slice_data.max() - slice_data.min()) * 255).astype(np.uint8)
+                        pil_image = Image.fromarray(normalized_slice)
+                        img_byte_arr = io.BytesIO()
+                        pil_image.save(img_byte_arr, format='PNG')
+                        img_bytes = img_byte_arr.getvalue()
+                        file_name = (
+                            f"{split}_"
+                            + image_path.split('medimg/')[-1].replace('\\', '/') + f'_slice{index}'
+                        )
+                        data.append({
+                            'img_name': file_name,
+                            'image': img_bytes,
+                        })
+                        counter += 1
+                except Exception as e:
+                    print(f"Error processing NIfTI {image_path}: {e}")
 
-    except Exception as e:
-        print(f"Error processing {image_path}: {e}")
-"""
-    # Save data in batches
-    if counter > batch_size:
+            elif image_path.lower().endswith(image_extensions):
+                # Handle regular image files
+                with open(image_path, 'rb') as f:
+                    img_bytes = f.read()
+                file_name = str(split) + '_' + image_path.split('medimg/')[-1].replace('\\', '/')
+                data.append({
+                    'img_name': file_name,
+                    'image': img_bytes,
+                })
+                counter += 1
+
+        except Exception as e:
+            print(f"Error processing {image_path}: {e}")
+
+        # Save data in batches
+        if counter > batch_size:
+            table = pa.Table.from_pandas(pd.DataFrame(data))
+            pq.write_table(table, parquet_file, row_group_size=100)
+
+            api.upload_file(
+                path_or_fileobj=parquet_file,
+                path_in_repo=f"data/{split}/{parquet_file}",
+                repo_id=repo_id,
+                repo_type='dataset',
+            )
+            print(f"Batch {split} uploaded to https://huggingface.co/datasets/{repo_id}/data/{split}/{parquet_file}")
+
+            # Clean up
+            if os.path.exists(parquet_file):
+                os.remove(parquet_file)
+            data = []
+            split += 1
+            counter = 0
+
+    # Save any remaining data
+    if counter > 0:
         table = pa.Table.from_pandas(pd.DataFrame(data))
-        pq.write_table(table, parquet_file, row_group_size=100)
+        pq.write_table(table, parquet_file)
 
         api.upload_file(
             path_or_fileobj=parquet_file,
@@ -139,29 +153,34 @@ for image_path in tqdm(image_file_paths, desc='Reading'):
             repo_id=repo_id,
             repo_type='dataset',
         )
-        print(f"Batch {split} uploaded to https://huggingface.co/datasets/{repo_id}/data/{split}/{parquet_file}")
+        print(f"Batch {split} uploaded to https://huggingface.co/datasets/{repo_id}")
 
         # Clean up
         if os.path.exists(parquet_file):
             os.remove(parquet_file)
-        data = []
-        split += 1
-        counter = 0
 
-# Save any remaining data
-if counter > 0:
-    table = pa.Table.from_pandas(pd.DataFrame(data))
-    pq.write_table(table, parquet_file)
 
-    api.upload_file(
-        path_or_fileobj=parquet_file,
-        path_in_repo=f"data/{split}/{parquet_file}",
-        repo_id=repo_id,
-        repo_type='dataset',
-    )
-    print(f"Batch {split} uploaded to https://huggingface.co/datasets/{repo_id}")
+def get_args():
+    parser = argparse.ArgumentParser(description='Upload images to Hugging Face Datasets')
+    parser.add_argument('--repo_id', type=str, default='GleghornLab/MedIMG', help='Hugging Face repository ID')
+    parser.add_argument('--batch_size', type=int, default=10000, help='Number of images to upload in each batch')
+    parser.add_argument('--base_path', type=str, default='E:/medimg', help='Base path to search for images')
+    parser.add_argument('--split', type=int, default=1, help='Starting split number')
+    return parser.parse_args()
 
-    # Clean up
-    if os.path.exists(parquet_file):
-        os.remove(parquet_file)
-"""
+
+if __name__ == '__main__':
+    args = get_args()
+    folders = [ex.replace('\\', '/') for ex in glob(f'{args.repo_id}*') if not ex.endswith('z01')]
+    print(folders)
+    args.image_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.dcm')
+    args.medical_image_extensions = ('.mhd', '.nii')
+    
+    args.parquet_file = 'images.parquet'
+    args.counter = 0
+    # login if needed
+    # from huggingface_hub import login
+    # login()
+    api = HfApi()
+    create_repo(repo_id=args.repo_id, repo_type='dataset', exist_ok=True)
+    main(args, api)
